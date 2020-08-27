8dee93d69f57d14fcfca1d0ae9cec8c49bfd8ae9
HiroHara-Nextremer <hiroaki.hara@nextremer.com>
2020-08-27 10:42:38 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const app = new Koa();
+const port = 3000;
+
+console.log(`node env = ${app.env}`);
+app.use(async ctx => {
+  ctx.body = 'hello there';
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
